d5643211d49b69c4a2d05b995d1f8a5ba59d08bc
pavol.karas@gmail.com <pavol.karas@gmail.com>
2018-11-29 22:17:48 +0100

    deleting unrelated files

--- script2.js
+++
@@ -1,9 +0,0 @@
-const largeNumber = 3654;
-
-// node did not have 'export' implemented the new ES6 feature of import-export statements.
-// export default largeNumber;
-
-// instead it uses:
-module.exports = {
-	largeNumber: largeNumber
-}
